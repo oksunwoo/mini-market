e7cba8ba8ef07a7ef03f8944de84a400714ce59f
oksunwoo <oksunwo@naver.com>
2022-05-22 21:06:16 +0900

    feat: Parse Error 타입 추가

---
+++ mini-market/mini-market/Error/JSONParseError.swift
@@ -0,0 +1,12 @@
+//
+//  JSONParseError.swift
+//  mini-market
+//
+//  Created by Sunwoo on 2022/05/22.
+//
+
+import Foundation
+
+enum JSONParseError: Error {
+    case decodingFail
+}
---
+++ mini-market/mini-market/Model/JSONParser.swift
@@ -0,0 +1,13 @@
+//
+//  JSONParser.swift
+//  mini-market
+//
+//  Created by Sunwoo on 2022/05/22.
+//
+
+import Foundation
+
+struct JSONParser<Item: Codable> {
+ 
+    
+}
