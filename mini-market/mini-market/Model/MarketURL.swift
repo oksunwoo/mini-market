a5294820da1c942aee74dc95b88363936caa9533
oksunwoo <oksunwo@naver.com>
2022-05-25 15:47:15 +0900

    refactor: network 메서드 역할분리

---
+++ mini-market/mini-market/Model/MarketURL.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  mini-market
+//
+//  Created by Sunwoo on 2022/05/25.
+//
+
+import Foundation
---
+++ mini-market/mini-market/URLProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  URLProtocol.swift
+//  mini-market
+//
+//  Created by Sunwoo on 2022/05/25.
+//
+
+import Foundation
