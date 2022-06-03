322a7f04ffd8199e57d0992b0d6c490fb17bce1d
oksunwoo <oksunwo@naver.com>
2022-06-04 01:30:18 +0900

    feat: 테이블뷰 기본 메서드 추가

--- mini-market/mini-market/ListViewController.swift
+++ mini-market/mini-market/ListViewController.swift
@@ -8,11 +8,31 @@
 import UIKit
 
 class ListViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
 
     }
+    
+}
 
+//MARK: - UITableViewDelegate
+extension ListViewController: UITableViewDelegate {
+    
+}
+
+//MARK: - UITableViewDataSource
+extension ListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
 }
