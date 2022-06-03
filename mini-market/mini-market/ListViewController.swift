ecc255fd3b367f1c220bcc1a8828cb9744112a56
oksunwoo <oksunwo@naver.com>
2022-06-04 00:04:37 +0900

    refactor: 불필요한 메서드 및 뷰컨트롤러 네이밍 변경

---
+++ mini-market/mini-market/ListViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  mini-market
+//
+//  Created by Sunwoo on 2022/04/18.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+
+    }
+
+}
