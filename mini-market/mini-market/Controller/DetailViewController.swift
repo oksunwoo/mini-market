93f1b66cb9a04980970a91c45ca5890f23b4cfa1
oksunwoo <oksunwo@naver.com>
2022-07-12 19:58:44 +0900

    feat: detailView 구성

--- mini-market/mini-market/Controller/DetailViewController.swift
+++ mini-market/mini-market/Controller/DetailViewController.swift
@@ -8,18 +8,23 @@
 import UIKit
 
 final class DetailViewController: UIViewController {
-    private var productID: Int?
-    private var productDetail: ProductDetail?
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productStockLabel: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+    @IBOutlet weak var productDiscountedLabel: UILabel!
+    
+    private var product: Product?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        setNavigationTitle()
-        setProduct()
+        setView(with: product)
+        
     }
     
-    init?(coder: NSCoder, productID: Int?) {
-        self.productID = productID
+    init?(coder: NSCoder, product: Product?) {
+        self.product = product
         super.init(coder: coder)
     }
     
@@ -27,17 +32,40 @@ final class DetailViewController: UIViewController {
         fatalError("init(coder:) has not been implemented")
     }
     
-    private func setNavigationTitle() {
-        navigationItem.title = String(productID!)
-    }
-    
-    private func setProduct() {
-        guard let id = productID else {
+    private func setView(with product: Product?){
+        guard let product = product else {
             return
         }
         
-        APIService().fetchData(api: ProductPageAPI(productID: id), decodingType: ProductDetail.self) { data in
-            self.productDetail = data
+        setNavigation(title: product.name)
+        setImageView(url: product.thumbnail)
+        setNameLabel(name: product.name)
+        setStockLabel(stock: product.stock)
+        setPriceLabel(price: product.price, discountedPrice: product.discountedPrice, currency: product.currency)
+    }
+    
+    private func setNavigation(title: String) {
+        navigationItem.title = title
+    }
+    
+    private func setImageView(url: String) {
+        productImageView.loadImage(url: url)
+    }
+    
+    private func setNameLabel(name: String) {
+        productNameLabel.text = name
+    }
+    
+    private func setStockLabel(stock: Int) {
+        productStockLabel.text = "수량: " + String(stock)
+    }
+    
+    private func setPriceLabel(price: Int, discountedPrice: Int, currency: Currency) {
+        if discountedPrice == 0 {
+            productPriceLabel.text = currency.rawValue + " " + price.formatted()
+        } else {
+            productPriceLabel.text = currency.rawValue + " " + price.formatted()
+            productDiscountedLabel.text = currency.rawValue + " " + discountedPrice.formatted()
         }
     }
 }
